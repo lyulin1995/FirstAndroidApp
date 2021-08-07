662f35f27173a5af6e8988d4385594d708d2cd79
Lin Lyu <l.lyu@student.tue.nl>
2021-08-07 03:06:17 +0200

    CheckBox and Toast Listener

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mBtnButtion;   //    声明控件
     private Button mBtnEditText;
     private Button mBtnRadioButton;
+    private Button mBtnCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         mBtnButtion = findViewById(R.id.btn_button);  // 找到控件
         mBtnEditText = findViewById(R.id.btn_edittext);
         mBtnRadioButton = findViewById(R.id.btn_radiobutton);
+        mBtnCheckBox = findViewById(R.id.btn_checkbox);
         setListeners();
     }
     // 设置监听事件接口
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         mBtnButtion.setOnClickListener(onClick);
         mBtnEditText.setOnClickListener(onClick);
         mBtnRadioButton.setOnClickListener(onClick);
+        mBtnCheckBox.setOnClickListener(onClick);
     }
 
     // 写一个class实现onClick的接口
@@ -45,17 +48,26 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = null;
             switch (v.getId()){
                 case R.id.btn_textview:
+                    // 跳转到TextView演示界面
                     intent = new Intent(MainActivity.this, TextViewActivity.class);
                     break;
                 case R.id.btn_button:
+                    // 跳转到Button演示界面
                     intent = new Intent(MainActivity.this, ButtonActivity.class);
                     break;
                 case R.id.btn_edittext:
+                    // 跳转到EditText演示界面
                     intent = new Intent(MainActivity.this, EditTextActivity.class);
                     break;
                 case R.id.btn_radiobutton:
+                    // 跳转到RadioButton演示界面
                     intent = new Intent(MainActivity.this, RadioButtonActivity.class);
                     break;
+                case R.id.btn_checkbox:
+                    // 跳转到Checkbox演示界面
+                    intent = new Intent(MainActivity.this, CheckBoxActivity.class);
+                    break;
+
             }
             startActivity(intent);
         }
